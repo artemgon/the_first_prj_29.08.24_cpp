62b8ef74051e25efd2e78a7098753909043ade31
artemgon <artemhoncharuk123@gmail.com>
2024-04-29 18:56:40 +0300

    Creating the first task of practice exercises.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+using namespace std;
 
 int main() {
+	int number;
+	cout << "Please, enter the number:";
+	cin >> number;
+	int i = 0;
+	while (number > i) {
+		cout << "\n" << i;
+		i++;
+	}
 	return 0;
 }
