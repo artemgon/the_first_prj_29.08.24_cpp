ba0680d6d7888e523401bce40e185fd96710f96f
artemgon <artemhoncharuk123@gmail.com>
2024-06-02 16:12:20 +0300

    Creating the fifth task of homework.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main() {
+	int a, i = 1;
+	cout << "Please, enter the multiplicand: ";
+	cin >> a;
+	while (i <= 9)
+	{
+		cout << "\n" << a << " x " << i << " = " << a * i;
+		i++;
+	}
 	return 0;
 }
