4c1a9881d4029221833688e819b6da7dc0530ae4
artemgon <artemhoncharuk123@gmail.com>
2024-06-02 16:03:48 +0300

    Creating the fourth task of homework.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main() {
+	int a;
+	double product = 1;
+	cout << "Please, enter the value of a(1 <= a <= 20): ";
+	cin >> a;
+	while (a <= 20)
+	{
+		product *= a;
+		a++;
+	}
+	cout << "The product is " << product;
 	return 0;
 }
