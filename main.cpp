4b4cb1e915badda9a4beac52481834079466918a
artemgon <artemhoncharuk123@gmail.com>
2024-06-02 14:48:16 +0300

    Creating the first task of homework.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
+	int a, sum = 0;
+	cout << "Please, enter the value of a: ";
+	cin >> a;
+	while (a <= 500)
+	{
+		sum += a;
+		a++;
+	}
+	cout << "The sum is " << sum;
 	return 0;
 }
