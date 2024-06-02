633472dcc077486b524e1744d0d5281c0271f2d2
artemgon <artemhoncharuk123@gmail.com>
2024-06-02 15:02:55 +0300

    Creating the second task of homework.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+	int x, y, product = 1, i = 1;
+	cout << "Please, enter the base of exponent: ";
+	cin >> x;
+	cout << "Please, enter the exponent: ";
+	cin >> y;
+	while (i <= y)
+	{
+		product *= x;
+		i++;
+	}
+	cout << "The product is " << product;
 	return 0;
 }
